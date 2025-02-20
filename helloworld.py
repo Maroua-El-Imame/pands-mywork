645a74fb774fa66d78203a7ef0e6ebd95a891691
Maroua El Imame <g00472980@atu.ie>
2025-02-20 19:44:46 +0000

    Hello mum updated

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,17 @@
-print ("Hello World!")
+# helloworld.py
+
+#  This program prints 'Hello mum'
+# 'Hello mum' x6 previously 'Hello world' that was updated using column selection mode
+
+
+
+print ("Hello Mum")
+print ("Hello Mum")
+print ("Hello Mum")
+print ("Hello Mum")
+print ("Hello Mum")
+print ("Hello Mum")
+
+
+
+
