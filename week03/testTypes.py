a3326611dd7bb24b3b73e79db7720a678b87f217
Maroua El Imame <g00472980@atu.ie>
2025-02-21 16:27:37 +0000

    week03

---
+++ week03/testTypes.py
@@ -0,0 +1,15 @@
+
+
+i=3
+fl=3.5
+isa=True
+memo = 'how now Brown Cow'
+lots = []
+
+
+
+print(f"variable 'i' is of type {type(i)} and value : {int(i)}")
+print(f"variable 'fl' is of type {type(fl)} and value : {float(fl)}")
+print(f"variable 'isa' a is of type {type(isa)} and value : {bool(isa)}")
+print(f"variable 'memo' is of type {type(memo)} and value : {str(memo)}")
+print(f"variable 'lots' is of type {type(lots)} and value : {list(lots)}")
