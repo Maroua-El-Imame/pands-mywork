5cc508a85b59c87362e9b85d144957659020a263
Maroua El Imame <g00472980@atu.ie>
2025-02-21 16:58:42 +0000

    week03

--- week03/testTypes.py
+++ week03/testTypes.py
@@ -1,3 +1,7 @@
+# testTypes.py
+# This programs prints types of variables
+# author Maroua 
+
 
 
 i=3
@@ -6,10 +10,9 @@ isa=True
 memo = 'how now Brown Cow'
 lots = []
 
+print(f'variable {'i'} is of type: {type(i)} and value:{(i)}'.format('i', type(i), i))
+print(f'variable {'fl'} is of type: {type(fl)} and value:{(fl)}'.format('i', type(i), i))
+print(f'variable {'isa'} is of type: {type(isa)} and value:{(isa)}'.format('i', type(i), i))
+print(f'variable {'memo'} is of type: {type(memo)} and value:{(memo)}'.format('i', type(i), i))
+print(f'variable {'lots'} is of type: {type(lots)} and value:{(lots)}'.format('i', type(i), i))
 
-
-print(f"variable 'i' is of type {type(i)} and value : {int(i)}")
-print(f"variable 'fl' is of type {type(fl)} and value : {float(fl)}")
-print(f"variable 'isa' a is of type {type(isa)} and value : {bool(isa)}")
-print(f"variable 'memo' is of type {type(memo)} and value : {str(memo)}")
-print(f"variable 'lots' is of type {type(lots)} and value : {list(lots)}")
