82c9eb8a43012bfeafd677f81ad8975275e327b9
Maroua El Imame <g00472980@atu.ie>
2025-03-05 22:47:13 +0000

    lab 4.1.4

---
+++ week04/whileLoopWeek4.py
@@ -0,0 +1,12 @@
+# whileLoopWeek4.py
+# This program uses a while loop, it keeps prompting the user for a number until the user enters -1.
+# Author : Maroua El Imame
+
+
+number= input("Please enter a number ")
+
+while number !="-1":
+    print ("The number you entered is incorrect")
+    number= input("Please enter a number ")
+
+print ("Correct ")
