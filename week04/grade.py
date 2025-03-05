03c3e7f941fbc73c3271c1feb20c723b07fd21e1
Maroua El Imame <g00472980@atu.ie>
2025-03-05 17:30:54 +0000

    lab 4.1.2

--- week04/grade.py
+++ week04/grade.py
@@ -1,4 +1,33 @@
-This program reads in
-# a students percentage
-# and prints out the
-# corresponding grade
+# grade.py
+# This program reads in a students percentage and prints out the corresponding grade
+# Author : Maroua EL imame
+
+'''
+Ref : 
+Under 40% => Fail
+Between 40% and 49% => Pass
+Between 50% and 59% => Merit 2
+Between 60% and 69% => Merit 1
+Over 70% => Distinction
+'''
+
+percentage = float(input("Enter the percentage: "))
+#print (percentage)
+
+# be careful with ands and ors
+if percentage < 0 or percentage > 100:
+ # Later we will show you error handling
+ # This should really throw an error
+    print ("Please enter a number between 0 and 100")
+elif percentage < 40:
+  # we know it is greater than 0
+    print ("Fail")
+elif percentage < 50: 
+ # between 40 and 49
+    print ("Pass")
+elif percentage < 60: # between 50 and 59
+    print ("Merit1")
+elif percentage < 70: # between 60 and 69
+    print ("Merit2")
+else: # the only option left
+     print ("Distinction")
