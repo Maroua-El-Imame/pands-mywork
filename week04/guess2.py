45f68e8cc897e1b954453720b06df477781d5c72
Maroua El Imame <g00472980@atu.ie>
2025-03-06 17:06:18 +0000

    lab 4.2.3/4

---
+++ week04/guess2.py
@@ -0,0 +1,20 @@
+# guess2.py
+# this program tells the user if their guess is too high or too low, each time they guess the number. #
+#  HINT: put an if statement inside the while loop
+'''
+numberToGuess = 30
+
+guess = int(input("Please guess the number:"))
+while guess != numberToGuess:
+    if guess <30:
+        print ("Too Low")    
+    else:
+        print("too high")
+    guess = int(input("Please guess again:"))
+print("Well done! Yes the number was ", numberToGuess)
+
+### modify the program so it generates a randon number from 0 to 100.
+'''
+
+import random
+print(random.randrange(1,100)) 
