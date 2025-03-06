668ef3fdede75d383006ca688140c6628d0983f3
Maroua El Imame <g00472980@atu.ie>
2025-03-06 16:39:13 +0000

    lab 4.2.1

---
+++ week04/evens.py
@@ -0,0 +1,11 @@
+# evens.py
+# This program prints out all the even numbers from 2 to 100
+# Author: Maroua EL imame
+
+
+numberTo = 100
+evenNum = 2
+
+while evenNum <= numberTo:
+    print (evenNum)
+    evenNum += 2
