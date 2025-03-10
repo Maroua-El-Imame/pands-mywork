d00fd25068934937a4df6c63e5906f655dedead7
Maroua El Imame <g00472980@atu.ie>
2025-03-10 14:42:02 +0000

    lab 5.2

---
+++ week05/tupleMonthsofYear.py
@@ -0,0 +1,12 @@
+# tupleMonthsofYear.py
+# this tuple stores the months of the year
+# from this tuple, we create another tuple with just the summer months (May, June, July)
+# # print out the summer months one at a time.
+
+
+months = ("January"," February "," March" ," April "," May " ," June " ," July ", "August", " September", "October", " November","December")
+summer = months[4:7]
+for month in summer:
+    print (month)
+
+
