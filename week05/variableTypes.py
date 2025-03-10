e0967d7ec829cabcc3386d393f7463da84c48a58
Maroua El Imame <g00472980@atu.ie>
2025-03-10 14:41:25 +0000

    lab 5.1

---
+++ week05/variableTypes.py
@@ -0,0 +1,45 @@
+# variableTypes.py
+# Quizz to identify variable types within a python program
+
+
+
+numberOfQuestions = 5
+averageAge = 23.4
+debugMode = True
+name = "joe"
+ages = []
+months = ('Jan', 'Feb', 'Mar')
+book = {}
+stuff = [ 12 , 'Fred', False, {}]
+someone = dict(firstname = "joe")
+me = {
+    "firstName" : "Andrew",
+    "teaching" : [{
+    "courseName" : "programming",
+    "semester" : 1
+    },{
+    "courseName" : "Data Representation",
+    "semester" : 2
+    }
+    ]
+    }
+
+# What are the variable types of the following variables in the code above
+#a. numberOfQuestions                             integer                               
+#b. averageAge                                    float               
+#c. debugMode                                     boolean             
+#d. name                                          string                  
+#e. ages                                          list      
+#f. months                                        tuple
+#g. months[1]                                     string
+#h. book                                          dictionnary
+#i. stuff                                       
+#j. stuff[2]                 dictionary                     
+#k. someone                                     
+#l. someone["firstname"]                                        
+#m. me                                      
+#n. me["teaching"]                                      
+#o. me["teaching"][0]["semester"]                                       
+#p is a trick question look at it carefully                                     
+#p. me["teaching"][0]["coursename"]                                     
+
