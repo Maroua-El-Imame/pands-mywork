0c08a6c8a59deedde8a55dc8ff9fde2bf1a3574b
Maroua El Imame <g00472980@atu.ie>
2025-04-01 23:48:38 +0100

    lab 6.3

---
+++ Week06/loopMenu.py
@@ -0,0 +1,29 @@
+# this program keeps displaying the menu util the user picks q.
+# if the user chooses a then call a function called doAdd() 
+# if the user chooses v then call a function called doView().
+
+# definening a function called menu using print
+def fromMenu():
+    print("What would you like to do?")
+    print("(a) Add new student")
+    print("(v) View students")
+    print("(q) Quit")
+    choice = input("Type one letter a or v or q :  ")
+    return choice
+def doAdd():
+    print(" add a student")
+    return choice
+def doView():
+    print("view student")
+    return choice
+
+
+#testing the function
+# make a variable out of the argument needed
+choice = fromMenu()
+if choice == "(a)":
+    doAdd()
+elif choice == "(v)":
+    doView()
+elif choice == "(q)":
+    print("quit")
