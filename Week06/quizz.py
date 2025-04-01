4c506b3c34920f9ce57f4bc0a1560065496a40f4
Maroua El Imame <g00472980@atu.ie>
2025-04-01 22:26:46 +0100

    lab 6.l

---
+++ Week06/quizz.py
@@ -0,0 +1,24 @@
+
+
+# function yo takes one parameter
+def yo(a):
+ return a * 2
+# here we are calling the function yo
+# and are passing in an argument, 3
+yo(3)
+
+'''
+Question :
+Look at the program above:
+a. What will be printed out when this program is run?
+b. What would you have to add to the program to get it to print out what 
+the calling the function yo(3) returns?'
+'''
+''''
+Answer:
+a. Nothing will be printed out when this program is run.
+reason : we did define the function but didnt execute it
+b. for this function to run, it should be preceeded by print()
+
+'''
+print(yo(3))
